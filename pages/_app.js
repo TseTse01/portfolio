9e40aaa6a6a2b59cfcd80c507e2b4af2cce3e86d
davit tsetsadze <tsetsedavit3@gmail.com>
2024-08-26 15:17:23 +0200

    start

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Davit Tsetsadze</title>
       </Head>
       <Component {...pageProps} />
     </>
