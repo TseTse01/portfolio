c1e17a679d09c5801471cec006de9e79787aaa60
davit tsetsadze <tsetsedavit3@gmail.com>
2024-09-07 01:49:38 +0200

    change componants profil

--- components/DownloadCVButton.js
+++ components/DownloadCVButton.js
@@ -1,24 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const DownloadCVButton = () => {
-  const [isClient, setIsClient] = useState(false);
+  const handleDownload = () => {
+    const fileUrl = "/cv.pdf";
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.setAttribute("download", "cv.pdf");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   useEffect(() => {
-    // Ce code s'exécute uniquement côté client
-    setIsClient(true);
+    // Ce code s'exécute uniquement côté client, `document` est sûr d'être défini ici
   }, []);
-
-  const handleDownload = () => {
-    if (isClient) {
-      const fileUrl = "/cv.pdf";
-      const link = document.createElement("a");
-      link.href = fileUrl;
-      link.setAttribute("download", "cv.pdf");
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  };
 
   return (
     <button onClick={handleDownload} className="download-btn">
--- components/Profil.js
+++ components/Profil.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
-// import DownloadCVButton from "./DownloadCVButton";
+// Importation dynamique du bouton pour désactiver le SSR
 import dynamic from "next/dynamic";
 
+// Désactivation du SSR pour DownloadCVButton
 const DownloadCVButton = dynamic(() => import("./DownloadCVButton"), {
   ssr: false,
 });
@@ -26,13 +27,14 @@ const Profil = () => {
             Après de nombreuses années dans des secteurs aussi différents que
             passionnants j’ai décidé de donner un nouveau souffle à ma carrière
             et d’oser vivre un rêve : Développeur Web. J’ai commencé par une
-            première formation en ligne FromScratch pour apprendre des base et
-            enchaîné avec une formation intensive de 2 mois et demie pour un
+            première formation en ligne FromScratch pour apprendre des bases et
+            enchaîné avec une formation intensive de 2 mois et demi pour un
             titre de développeur Web/Mobile RNCP niveau 6 avec la formation La
             Capsule. Passionné avant tout, je réalise de nombreux
             “side-projects” pour monter en compétence, et découvrir de nouveaux
             outils, mais soyons honnête... pour m'amuser aussi !
           </p>
+          {/* Le bouton de téléchargement du CV */}
           <DownloadCVButton />
         </div>
       </div>
